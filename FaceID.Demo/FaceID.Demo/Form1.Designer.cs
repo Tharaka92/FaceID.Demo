8648cc9a5048518da07617a56afa8bd83ca4c8bc
Tharaka Herath <tashkraze@gmail.com>
2020-04-27 21:48:54 +0530

    Repo init

---
+++ FaceID.Demo/FaceID.Demo/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace FaceID.Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deviceCmb = new System.Windows.Forms.ComboBox();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.DeviceLbl = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deviceCmb
+            // 
+            this.deviceCmb.FormattingEnabled = true;
+            this.deviceCmb.Location = new System.Drawing.Point(79, 22);
+            this.deviceCmb.Name = "deviceCmb";
+            this.deviceCmb.Size = new System.Drawing.Size(391, 21);
+            this.deviceCmb.TabIndex = 0;
+            this.deviceCmb.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(569, 91);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 23);
+            this.btnDetect.TabIndex = 1;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // DeviceLbl
+            // 
+            this.DeviceLbl.AutoSize = true;
+            this.DeviceLbl.Location = new System.Drawing.Point(38, 25);
+            this.DeviceLbl.Name = "DeviceLbl";
+            this.DeviceLbl.Size = new System.Drawing.Size(41, 13);
+            this.DeviceLbl.TabIndex = 2;
+            this.DeviceLbl.Text = "Device";
+            this.DeviceLbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(12, 66);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(541, 347);
+            this.pic.TabIndex = 3;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 450);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.DeviceLbl);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.deviceCmb);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Face Detection Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox deviceCmb;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Label DeviceLbl;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ FaceID.Demo/FaceID.Demo/Form1.cs
@@ -0,0 +1,79 @@
+﻿using AForge.Video.DirectShow;
+using AForge.Video;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceID.Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                deviceCmb.Items.Add(device.Name);
+            deviceCmb.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[deviceCmb.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pic.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning)
+                device.Stop();
+        }
+    }
+}
