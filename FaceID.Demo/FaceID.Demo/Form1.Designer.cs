2e403b7766fa4f493ab45b5d6a3a3cc379c2b29e
Tharaka Herath <tashkraze@gmail.com>
2020-04-28 20:37:46 +0530

    Face recognition added.

--- FaceID.Demo/FaceID.Demo/Form1.Designer.cs
+++ FaceID.Demo/FaceID.Demo/Form1.Designer.cs
@@ -28,25 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.deviceCmb = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
             this.btnDetect = new System.Windows.Forms.Button();
-            this.DeviceLbl = new System.Windows.Forms.Label();
-            this.pic = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.messageLbl = new System.Windows.Forms.Label();
+            this.nameDisplayLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.registerbtn = new System.Windows.Forms.Button();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
             this.SuspendLayout();
-            // 
-            // deviceCmb
-            // 
-            this.deviceCmb.FormattingEnabled = true;
-            this.deviceCmb.Location = new System.Drawing.Point(79, 22);
-            this.deviceCmb.Name = "deviceCmb";
-            this.deviceCmb.Size = new System.Drawing.Size(391, 21);
-            this.deviceCmb.TabIndex = 0;
-            this.deviceCmb.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // btnDetect
             // 
-            this.btnDetect.Location = new System.Drawing.Point(569, 91);
+            this.btnDetect.Location = new System.Drawing.Point(136, 312);
             this.btnDetect.Name = "btnDetect";
             this.btnDetect.Size = new System.Drawing.Size(75, 23);
             this.btnDetect.TabIndex = 1;
@@ -54,51 +51,100 @@
             this.btnDetect.UseVisualStyleBackColor = true;
             this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
             // 
-            // DeviceLbl
+            // messageLbl
             // 
-            this.DeviceLbl.AutoSize = true;
-            this.DeviceLbl.Location = new System.Drawing.Point(38, 25);
-            this.DeviceLbl.Name = "DeviceLbl";
-            this.DeviceLbl.Size = new System.Drawing.Size(41, 13);
-            this.DeviceLbl.TabIndex = 2;
-            this.DeviceLbl.Text = "Device";
-            this.DeviceLbl.Click += new System.EventHandler(this.label1_Click);
+            this.messageLbl.AutoSize = true;
+            this.messageLbl.Location = new System.Drawing.Point(299, 432);
+            this.messageLbl.Name = "messageLbl";
+            this.messageLbl.Size = new System.Drawing.Size(0, 13);
+            this.messageLbl.TabIndex = 4;
             // 
-            // pic
+            // nameDisplayLbl
             // 
-            this.pic.Location = new System.Drawing.Point(12, 66);
-            this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(541, 347);
-            this.pic.TabIndex = 3;
-            this.pic.TabStop = false;
+            this.nameDisplayLbl.AutoSize = true;
+            this.nameDisplayLbl.Location = new System.Drawing.Point(409, 317);
+            this.nameDisplayLbl.Name = "nameDisplayLbl";
+            this.nameDisplayLbl.Size = new System.Drawing.Size(43, 13);
+            this.nameDisplayLbl.TabIndex = 5;
+            this.nameDisplayLbl.Text = "Status: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(446, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Click Detect Button";
+            // 
+            // registerbtn
+            // 
+            this.registerbtn.Location = new System.Drawing.Point(560, 222);
+            this.registerbtn.Name = "registerbtn";
+            this.registerbtn.Size = new System.Drawing.Size(84, 23);
+            this.registerbtn.TabIndex = 7;
+            this.registerbtn.Text = "Register";
+            this.registerbtn.UseVisualStyleBackColor = true;
+            this.registerbtn.Click += new System.EventHandler(this.registerbtn_Click);
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(394, 222);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(146, 20);
+            this.nameTxt.TabIndex = 8;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(460, 93);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(100, 100);
+            this.imageBox1.TabIndex = 9;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBoxFrameGrabber
+            // 
+            this.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(12, 39);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(330, 240);
+            this.imageBoxFrameGrabber.TabIndex = 10;
+            this.imageBoxFrameGrabber.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(656, 450);
-            this.Controls.Add(this.pic);
-            this.Controls.Add(this.DeviceLbl);
+            this.Controls.Add(this.imageBoxFrameGrabber);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.registerbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameDisplayLbl);
+            this.Controls.Add(this.messageLbl);
             this.Controls.Add(this.btnDetect);
-            this.Controls.Add(this.deviceCmb);
             this.Name = "Form1";
             this.RightToLeftLayout = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Face Detection Demo";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox deviceCmb;
         private System.Windows.Forms.Button btnDetect;
-        private System.Windows.Forms.Label DeviceLbl;
-        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label messageLbl;
+        private System.Windows.Forms.Label nameDisplayLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button registerbtn;
+        private System.Windows.Forms.TextBox nameTxt;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
     }
 }
 
